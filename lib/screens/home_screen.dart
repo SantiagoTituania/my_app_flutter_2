3333b74a66381949173023a8f04dc99820edd00d
SantiagoTituania <santiago.tituania93@gmail.com>
2023-01-03 15:39:38 -0500

    Cambios en nueva app

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('MI SEGUNDA APP'),
+          actions: [
+    
+        PopupMenuButton(
+          // add icon, by default "3 dot" icon
+          // icon: Icon(Icons.book)
+          itemBuilder: (context){
+            return [
+              PopupMenuItem<int>(
+                    value: 0,
+                      child: Text("My Account.."),
+                  ),
+
+                  PopupMenuItem<int>(
+                      value: 1,
+                      child: Text("Settings"),
+                  ),
+
+                  PopupMenuItem<int>(
+                      value: 2,
+                      child: Text("Logout"),
+                  ),
+              ];
+          },
+          onSelected:(value){
+            if(value == 0){
+                print("My account menu is selected.");
+            }else if(value == 1){
+                print("Settings menu is selected.");
+            }else if(value == 2){
+                print("Logout menu is selected.");
+            }
+          }
+        ),
+
+  ],
+        ),
+        body: const Center(child: Text("Hola de nuevo, mundo"),),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(height: 100.5),
+        ),
+      );
+  }
+
+}
