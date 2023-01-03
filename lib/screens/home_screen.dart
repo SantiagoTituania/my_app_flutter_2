2e757d5dca4ac81812c91f0a7bbfde02b63e88db
SantiagoTituania <santiago.tituania93@gmail.com>
2023-01-03 15:58:02 -0500

    Cambio de color a popupMenu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget{
     return Scaffold(
         appBar: AppBar(
           title: const Text('MI SEGUNDA APP'),
+          backgroundColor: Colors.redAccent,
           actions: [
     
         PopupMenuButton(
